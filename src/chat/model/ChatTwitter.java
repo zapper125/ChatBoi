ff20305bc02c13a0d6128b70390926f758925fdf
Aaron Christiansen <125aaron@gmail.com>
2019-04-29 11:57:57 -0600

    stuff

--- src/chat/model/ChatTwitter.java
+++ src/chat/model/ChatTwitter.java
@@ -1,6 +1,17 @@
 package chat.model;
 
+import chat.controller.ChatController;
+import twitter4j.TwitterFactory;
+import twitter4j.Twitter;
+
 public class ChatTwitter
 {
-
+	private ChatController app;
+	private Twitter chatTwitter;
+	
+	public ChatTwitter(ChatController app) 
+	{
+		this.app = app;
+		this.chatTwitter = TwitterFactory.getSingleton();
+	}
 }
